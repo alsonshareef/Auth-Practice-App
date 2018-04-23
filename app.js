b756487d4b6711806a6f4479484580a4170e42d4
Alson Shareef <alson.shareef@gmail.com>
2018-04-23 21:25:55 +0800

    Completed configurations for Passport.js and Express sessions

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const localStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
 
-app.set('view engine', 'ejs');
+// MODELS
+const User = require("./models/user");
+
+// CONFIG ================================================
 mongoose.connect("mongodb://localhost/auth_app");
+app.set('view engine', 'ejs');
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(require("express-session")({
+    secret: "This is an app for practicing user authentication",
+    resave: false,
+    saveUninitialized: false 
+}));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // ROUTES ================================================
 app.get("/", function(req, res){
