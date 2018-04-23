0148e2255e61b7d4f792b7ddccad6719e9955299
Alson Shareef <alson.shareef@gmail.com>
2018-04-23 19:39:17 +0800

    Authentication app configuration

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+    res.render("home.ejs");
+});
+
+// =======================================================
+app.listen(3000, function(){
+    console.log("Server has started on port 3000!");
+});
