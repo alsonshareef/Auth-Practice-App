c0dca9cca32be54443f15a74604f7971a1c59eac
Alson Shareef <alson.shareef@gmail.com>
2018-04-23 19:58:48 +0800

    Added 'secret' route

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 app.set('view engine', 'ejs');
+mongoose.connect("mongodb://localhost/auth_app");
 
+// ROUTES ================================================
 app.get("/", function(req, res){
     res.render("home.ejs");
+});
+
+app.get("/secret", function(req, res){
+    res.render("secret.ejs");
 });
 
 // =======================================================
