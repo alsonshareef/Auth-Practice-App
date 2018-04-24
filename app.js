100c2774b00371a449b294873d2424ac95ecf002
Alson Shareef <alson.shareef@gmail.com>
2018-04-24 18:13:30 +0800

    Added registration form and POST route with sign up configs

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const User = require("./models/user");
 // CONFIG ================================================
 mongoose.connect("mongodb://localhost/auth_app");
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(require("express-session")({
@@ -29,6 +30,23 @@ app.get("/", function(req, res){
 
 app.get("/secret", function(req, res){
     res.render("secret.ejs");
+});
+
+// Authentication Routes
+app.get("/register", function(req, res){
+    res.render("register.ejs");
+});
+
+app.post("/register", function(req, res){
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        })
+    });
 });
 
 // =======================================================
