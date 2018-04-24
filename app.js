d1e5accd9fe61d1b6e1a6a0f4628289a9d03f078
Alson Shareef <alson.shareef@gmail.com>
2018-04-24 18:56:34 +0800

    Added page links to all templates

--- app.js
+++ app.js
@@ -62,6 +62,8 @@ app.post("/login",passport.authenticate("local", {
 }), function(req, res){
 });
 
+
+
 // =======================================================
 app.listen(3000, function(){
     console.log("Server has started on port 3000!");
